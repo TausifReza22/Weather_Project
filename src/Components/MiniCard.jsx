751dc6f036aea4652a9144e3e598d0410b545063
TausifReza22 <tausif22@navgurukul.org>
2024-07-12 22:55:52 +0530

    modifi code

--- src/Components/MiniCard.jsx
+++ src/Components/MiniCard.jsx
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import sun from '../assets/icons/sun.png'
-import cloud from '../assets/icons/cloud.png'
-import fog from '../assets/icons/fog.png'
-import rain from '../assets/icons/rain.png'
-import snow from '../assets/icons/snow.png'
-import storm from '../assets/icons/storm.png'
-import wind from '../assets/icons/windy.png'
+import React, { useEffect, useState } from 'react';
+import sun from '../assets/icons/sun.png';
+import cloud from '../assets/icons/cloud.png';
+import fog from '../assets/icons/fog.png';
+import rain from '../assets/icons/rain.png';
+import snow from '../assets/icons/snow.png';
+import storm from '../assets/icons/storm.png';
+import wind from '../assets/icons/windy.png';
 
 const MiniCard = ({ time, temp, iconString }) => {
-  console.log(time)
-  const [icon, setIcon] = useState()
+  const [icon, setIcon] = useState();
 
   useEffect(() => {
     if (iconString) {
       if (iconString.toLowerCase().includes('cloud')) {
-        setIcon(cloud)
+        setIcon(cloud);
       } else if (iconString.toLowerCase().includes('rain')) {
-        setIcon(rain)
+        setIcon(rain);
       } else if (iconString.toLowerCase().includes('clear')) {
-        setIcon(sun)
+        setIcon(sun);
       } else if (iconString.toLowerCase().includes('thunder')) {
-        setIcon(storm)
+        setIcon(storm);
       } else if (iconString.toLowerCase().includes('fog')) {
-        setIcon(fog)
+        setIcon(fog);
       } else if (iconString.toLowerCase().includes('snow')) {
-        setIcon(snow)
+        setIcon(snow);
       } else if (iconString.toLowerCase().includes('wind')) {
-        setIcon(wind)
+        setIcon(wind);
       }
     }
-  }, [iconString])
+  }, [iconString]);
 
+  const formatTemp = (celsius) => {
+    const fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit.toFixed(2);
+  }
 
   return (
     <div className='glassCard w-[10rem] h-[10rem] p-4 flex flex-col'>
       <p className='text-center'>
         {new Date(time).toLocaleTimeString('en', { weekday: 'long' }).split(" ")[0]}
       </p>
-      {/* <br /> */}
       <div className='w-full flex justify-center items-center flex-1'>
         <img src={icon} alt="forecast not available" className='w-[4rem] h-[4rem]' />
       </div>
-      <p className='text-center font-bold'>{temp}&deg;C</p>
+      <p className='text-center font-bold'>{formatTemp(temp)}&deg;F</p>
     </div>
-  )
+  );
 }
 
-export default MiniCard
+export default MiniCard;
